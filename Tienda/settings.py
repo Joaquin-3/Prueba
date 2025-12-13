bc546bf9b148f1be89573baddf840b8eb231e51c
Joaquin-3 <joaquin.molina18@inacapmail.cl>
2025-12-13 14:10:14 -0300

    deploy nose1

--- Tienda/settings.py
+++ Tienda/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.onrender.com']
 
 # Application definition
 
